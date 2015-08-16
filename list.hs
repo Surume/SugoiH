4ee14c6fa2d6e0f617c8f7b4908526626d7956a3
surume <mail@sururne.com>
2015-08-16 22:28:17 +0900

    [Add] a_small_cat

--- list.hs
+++ list.hs
@@ -2,3 +2,5 @@ lostNumbers = [4,8,15,16,23,42]
 renketsu = [1,2,3,4] ++ [9,10,11,12]
 moji_renketsu = "hello" ++ " " ++ "world"
 moji_renketsu2 = ['w','o'] ++ ['o','t']
+a_small_cat = 'A':" SMALL CAT"
+cons_int = 5:[1,2,3,4,5]
