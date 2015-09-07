73a06645905a497b1adaa66bcc5a5d87a47c017d
Shota Miyamoto <miyamoto@feedforce.jp>
2015-09-07 13:59:16 +0900

    Update bmiTell

--- guard.hs
+++ guard.hs
@@ -1,7 +1,6 @@
-bmiTell :: Double -> String
-bmiTell bmi
-    | bmi <= 18.5 = "Youre underweight, you emo, you!"
-    | bmi <= 25.0 = "Youre supporsedly nomal"
-    | bmi <= 30.0 = "Youre fat!"
+bmiTell :: Double -> Double -> String
+bmiTell weight height
+    | weight / height ^ 2 <= 18.5 = "Youre underweight, you emo, you!"
+    | weight / height ^ 2 <= 25.0 = "Youre supporsedly nomal"
+    | weight / height ^ 2 <= 30.0 = "Youre fat!"
     | otherwise   = "Youre a whale congratulations"
-
