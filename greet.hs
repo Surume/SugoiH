0ab2f9893151cddfaaf3315143d59966d1cf1522
Shota Miyamoto <miyamoto@feedforce.jp>
2015-09-08 14:13:10 +0900

    Refactor greet.hs

--- greet.hs
+++ greet.hs
@@ -1,6 +1,9 @@
+niceGreeting :: String
+niceGreeting = "Hello! So very nice to see you,"
+badGreeting :: String
+badGreeting = "Oh! Pfft. It's you."
+
 greet :: String -> String
 greet "Juan" = niceGreeting ++ " Juan!"
 greet "Fernand" = niceGreeting ++ " Fernand!"
 greet name = badGreeting ++ " " ++ name
-    where niceGreeting = "Hello! So very nice to see you,"
-          badGreeting = "Oh! Pfft. It's you."
