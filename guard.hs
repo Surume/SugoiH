0ae86c7eea9b5036e086f397be4882876c9b389a
Shota Miyamoto <miyamoto@feedforce.jp>
2015-09-08 13:55:48 +0900

    Use where

--- guard.hs
+++ guard.hs
@@ -1,9 +1,10 @@
 bmiTell :: Double -> Double -> String
 bmiTell weight height
-    | weight / height ^ 2 <= 18.5 = "Youre underweight, you emo, you!"
-    | weight / height ^ 2 <= 25.0 = "Youre supporsedly nomal"
-    | weight / height ^ 2 <= 30.0 = "Youre fat!"
+    | bmi <= 18.5 = "Youre underweight, you emo, you!"
+    | bmi <= 25.0 = "Youre supporsedly nomal"
+    | bmi <= 30.0 = "Youre fat!"
     | otherwise   = "Youre a whale congratulations"
+    where bmi = weight / height ^ 2
 
 max' :: (Ord a) => a -> a -> a
 max' a b
